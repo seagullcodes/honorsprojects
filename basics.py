a49852f7b494b7c44c303935eae691bdae1b0adc
seagullcodes <sarahsiegel26@marlborough.org>
2025-08-26 19:58:18 -0700

    first projects commit!

---
+++ basics.py
@@ -0,0 +1,58 @@
+#this is a super cool comment
+#list is 0 index based
+
+
+#below this is an example for loop
+things = [7, 4.20, "juggling ball", True]
+
+for item in things:
+    print("item:", item)
+
+#range() function
+
+#elif is else if
+#if something == 0:
+#for in in range
+print("this is multiples of multiple of three")
+
+
+x=0
+count=0
+for count in range(10):
+    print(x)
+    x+=3
+    count+1
+
+print("this is my fizzbuzz")
+
+for i in range (31):
+    if i%3 == 0 and i%5 == 0:
+        print("Fizzbuz")
+    elif i%3 == 0:
+        print("Fizz")
+    elif i%5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+print("geometric sequence")
+
+for c in range(40):
+    print(i) 
+    i *= 2
+
+
+print("these cubes")
+
+for i in range(10):
+    print(i*i*i)
+    
+
+print("fib")
+a=0
+b=1
+for l in range(2):
+    l = a+b
+    print(l)
+    a=b
+    b=l
